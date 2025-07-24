98a1083336b4a36d0c44a0e48808454f6e42dd0b
Joyce <gathiithijoyce74@gmail.com>
2025-07-25 00:22:10 +0300

    Change your view to use the correct template path

--- quotes/views.py
+++ quotes/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from quotes.models import Quote
 def random_quote(request):
     quote = Quote.objects.order_by('?').first()  # Get a random quote
-    return render(request,'quote.html',{'quote': quote})  # Pass the quote to the template
-    
+    return render(request, 'quotes/quote.html', {'quote': quote})
