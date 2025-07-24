42f42f2c016aabe29ad33758ce6815cf5ac24a41
Joyce <gathiithijoyce74@gmail.com>
2025-07-25 00:03:09 +0300

    update url.py of app quotes to define root

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from quotes.views import random_quote  # import your quote view
+
 urlpatterns = [
-    path('quote/', views.random_quote, name='random_quote'),
+    path('admin/', admin.site.urls),
+    path('quotes/', include('quotes.urls')),
+    path('', random_quote, name='home'),  # 👈 this serves the root URL
 ]
